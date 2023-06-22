7b184dc0161955febc75fe0dc862dcf824216a55
Robert Tucker <r.tucker@quadient.com>
2023-06-22 11:29:57 -0400

    refactor: init with config instead of shared disk

--- qar-home/qar/scripts/generate-rest-config.sh
+++ qar-home/qar/scripts/generate-rest-config.sh
@@ -3,55 +3,75 @@ set -euo pipefail
 
 # Deploy existing configration
 deployConfig() {
-  echo "${REST_CONFIG_TAR_FILE} exists. Deploying configuration."
+  echo "Deploying stored configuration."
 
-  # Un-tar the configuration file in the appropriate location
-  tar -xvf "${REST_CONFIG_TAR_FILE}" -C "${QAR_HOME}"/tmp
+  # Un-tar the base64-encoded configuration file in the appropriate location
+  base64 -d "${RESTCFG_FILE}" | tar -xv -C "${RESTCFG_WORK}"
 }
 
 # Generate new configuration
 generateConfig() {
-  echo "${REST_CONFIG_TAR_FILE} does not exist. Generating configuration."
+  echo "Generating REST API configuration."
+
+  RESTCFG_DIR="${RESTCFG_WORK}"/restcfg
+  TEMPLATE_FILE=default.tmpl
 
   # Create the REST config directory and copy the template file
-  java -jar "${OD_HOME}"/jars/ODRESTAdmin.jar setup -configDir "${REST_CONFIG_DIR}"
-  cp "${QAR_HOME}"/config/rest/"${TEMPLATE_FILE}" "${REST_CONFIG_DIR}"
+  java -jar "${OD_HOME}"/jars/ODRESTAdmin.jar setup -configDir "${RESTCFG_DIR}"
+  cp "${QAR_HOME}"/config/rest/"${TEMPLATE_FILE}" "${RESTCFG_DIR}"
 
   # Populate the template file with instance parameters
-  sed -i "s|odHost=|odHost=${OD_HOST}|" "${REST_CONFIG_DIR}"/"${TEMPLATE_FILE}"
-  sed -i "s|odPort=|odPort=${OD_PORT}|" "${REST_CONFIG_DIR}"/"${TEMPLATE_FILE}"
-  sed -i "s|odUser=|odUser=${OD_USER}|" "${REST_CONFIG_DIR}"/"${TEMPLATE_FILE}"
-  sed -i "s|odPassword=|odPassword=${OD_PASSWORD}|" "${REST_CONFIG_DIR}"/"${TEMPLATE_FILE}"
-  sed -i "s|odInstance=|odInstance=${OD_INSTANCE_NAME}|" "${REST_CONFIG_DIR}"/"${TEMPLATE_FILE}"
+  sed -i "s|odHost=|odHost=${OD_HOST}|" "${RESTCFG_DIR}"/"${TEMPLATE_FILE}"
+  sed -i "s|odPort=|odPort=${OD_PORT}|" "${RESTCFG_DIR}"/"${TEMPLATE_FILE}"
+  sed -i "s|odUser=|odUser=${OD_USER}|" "${RESTCFG_DIR}"/"${TEMPLATE_FILE}"
+  sed -i "s|odPassword=|odPassword=${OD_PASSWORD}|" "${RESTCFG_DIR}"/"${TEMPLATE_FILE}"
+  sed -i "s|odInstance=|odInstance=${OD_INSTANCE_NAME}|" "${RESTCFG_DIR}"/"${TEMPLATE_FILE}"
 
-  # Generate the pool, save access keys and configuration
+  # Generate a REST connection pool
   java -jar "${OD_HOME}"/jars/ODRESTAdmin.jar createPool \
-    -configDir "${REST_CONFIG_DIR}" \
+    -configDir "${RESTCFG_DIR}" \
     -template "${TEMPLATE_FILE}" \
     -odInstance "${OD_INSTANCE_NAME}" \
     -odPassword "${OD_PASSWORD}" \
-    -poolName odpool \
+    -poolName "${REST_POOL_NAME}" \
     -odUser "${OD_USER}"
+
+  # Create an access key for the named user
   java -jar "${OD_HOME}"/jars/ODRESTAdmin.jar generateKey \
-    -configDir "${REST_CONFIG_DIR}" \
-    -poolName odpool \
-    -consumerName "${OD_USER}" \
-    >"${QAR_HOME}"/config/rest/odpool/"${OD_USER}".rest.credentials
-  tar -cvf "${REST_CONFIG_TAR_FILE}" -C "/opt/qar/tmp" restcfg
+    -configDir "${RESTCFG_DIR}" \
+    -poolName "${REST_POOL_NAME}" \
+    -consumerName "${REST_CONSUMER_NAME}"
+
+  # Remove the template file (has unencrypted password in it) before createing
+  # a base64-encoded tar file with the configuration.
+  rm -f "${RESTCFG_DIR}"/"${TEMPLATE_FILE}"
+  ENCODED_FILE="${RESTCFG_WORK}"/restcfg.enc
+  tar -cv -C "${RESTCFG_WORK}" restcfg | base64 -w 0 >"${ENCODED_FILE}"
+
+  # Store the encoded configuration in the ConfigMap
+  SERVICE_ACCOUNT="/var/run/secrets/kubernetes.io/serviceaccount"
+  NAMESPACE=$(cat "${SERVICE_ACCOUNT}/namespace")
+  curl -sS \
+    -X PATCH \
+    -H "Authorization: Bearer $(cat ${SERVICE_ACCOUNT}/token)" \
+    -H "Accept: application/json" \
+    -H "Content-Type: application/json-patch+json" \
+    --cacert "${SERVICE_ACCOUNT}/ca.crt" \
+    --data "[{\"op\":\"replace\",\"path\":\"/data\",\"value\":{\"restcfg.enc\":\"$(cat "${ENCODED_FILE}")\"}}]" \
+    "https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/${NAMESPACE}/configmaps/${RESTCFG_NAME}"
+
+  echo -e "\nFinished generating REST API configuration."
 }
 
 # --
 # -- Main
 # --
 
-# Define/export key variables
-REST_CONFIG_DIR="${QAR_HOME}"/tmp/restcfg
-TEMPLATE_FILE=default.tmpl
-REST_CONFIG_TAR_FILE="${QAR_HOME}"/config/rest/odpool/restcfg.tar
-export REST_CONFIG_DIR TEMPLATE_FILE REST_CONFIG_TAR_FILE
+RESTCFG_FILE="${QAR_HOME}"/config/rest/stored/restcfg.enc
+RESTCFG_WORK="${QAR_HOME}"/config/rest/work
 
 # Test for run conditions
-if [ -e "${REST_CONFIG_TAR_FILE}" ]; then
+if [ -e "${RESTCFG_FILE}" ]; then
   deployConfig
 else
   generateConfig
