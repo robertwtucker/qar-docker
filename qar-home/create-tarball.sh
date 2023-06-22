ac6afc1c08a1aafbc995f5a8c566b07f17eae84d
Robert Tucker <r.tucker@quadient.com>
2023-06-21 21:19:44 -0400

    feat: add simple version stamping for qar-home

--- qar-home/create-tarball.sh
+++ qar-home/create-tarball.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd $(dirname "$0") && echo "Creating tarball from $(PWD):"
+# Setup
+cd "$(dirname "$0")"
+echo "Creating tarball from $(PWD)..."
+
+# Add a simple version stamp
+TAG="$(awk -F= '/^tag/{print $2}' .release)"
+echo "Tagged version is ${TAG}"
+awk -F. '{print "0." $2 "." $3}' <<<"${TAG}" >qar/.version
+
+# Create the tarball
 gtar --create --overwrite --owner=1001 --group=0 --verbose --file=qar-home.tar qar
 echo "Done."
